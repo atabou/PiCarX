a1a8af85688eaef6a530ea7ce57eb205be527b01
atabou <andretabourian@hotmail.com>
2024-02-01 21:50:13 -0800

    Added filter and refactored code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "picarx.hpp"
 #include "pid.hpp"
+#include "filters.hpp"
+#include "utilities.hpp"
 
 #include <stdio.h>
 #include <unistd.h>
@@ -7,11 +9,112 @@
 #include <signal.h>
 
 
+#define FILTER_ALPHA_COEFF 1.0f
+#define LOG_DIFF_BIAS 0.25f
+
+
+/**
+ * @brief Gracefully exits the program.
+ * This function is called when the user presses Ctrl+C.
+ * It disconnects from PiCarX and exits the program.
+ * @param sig The signal number.
+ * @return void
+*/
+void gracefulExit(int sig);
+
+
 PiCarX* picarx = NULL;
 PIDController* PID = NULL;
+FIRFilter* filter = NULL;
+
+
+int main() {
+
+    // Register graceful exit handler
+    signal(SIGINT, gracefulExit);
+
+    // Create and connect PiCarX object
+    picarx = new PiCarX();
+
+    picarx->connect();
+
+    if (!picarx->isConnected()) {
+    
+        printf("Failed to connect to PiCarX\n");
+        return 1;
+    
+   }
+
+    // Initialize PID controller.
+    PID = new PIDController(15.0f, 0.0f, 0.0f, 0.1f);
+
+    // Initialize FIR filter by calculating the mean of the first 100 samples
+    float mu0 = 0.0f;
+
+    for (int i = 0; i < 100; i++) {
+
+        float left  = picarx->getAnalogVoltage(A0) * 5.7f;
+        float right = picarx->getAnalogVoltage(A3) * 5.7f;
+        
+        mu0 += logdiff(left, right, LOG_DIFF_BIAS);
+        usleep(1000); // TODO check the ability of the MCU to handle this sampling rate
+    
+    }
+
+    float mu0 /= 100.0f;
+
+    filter = new FIRFilter(FILTER_ALPHA_COEFF, mu0);
+
+    // Control loop
+    while (true) {
+
+        // Get battery voltage
+        float battery_voltage = picarx->getBatteryVoltage();
+	    
+        // Get analog voltage from A0 and A1
+        float left  = picarx->getAnalogVoltage(A0) * 5.7f;
+        float right = picarx->getAnalogVoltage(A3) * 5.7f;
+
+	    // Get log difference between left and right
+        float diff = logdiff(left, right, LOG_DIFF_BIAS);
+
+        // Verify the validity of the difference
+        if (!isnan(diff) && !isinf(diff)) {
+        
+            // Filter the log difference
+            float filtered = filter->pass(diff);
+
+            // Get pid response
+            float response = PID->pass(0.0f, filtered);
+
+            // Set steering angle
+            picarx->setSteeringAngle(response);
+        
+        }
+
+        usleep(100000);
+
+    }
+
+    if (picarx != NULL) {
+        picarx->disconnect();
+        delete picarx;
+        picarx = NULL;
+    }
+
+    if (PID != NULL) {
+        delete PID;
+        PID = NULL;
+    }
+
+    return 0;
+
+}
 
 
 void gracefulExit(int sig) {
+
+    printf("Gracefully exiting...\n");
 
     if (picarx != NULL) {
         picarx->disconnect();
@@ -27,112 +130,3 @@ void gracefulExit(int sig) {
     exit(0);
 
 }
-
-
-void run() {
-
-    while (true) {
-
-        // Get battery voltage
-        float battery_voltage = picarx->getBatteryVoltage();
-	printf("battery: %f V\n", battery_voltage);
-
-        // Get analog voltage from A0 and A1
-        float left  = picarx->getAnalogVoltage(A0) * 5.7f;
-        float right = picarx->getAnalogVoltage(A3) * 5.7f;
-
-	printf("left: %f V\n", left);
-	printf("right: %f V\n", right);
-        
-	// Get log difference between left and right
-        //float diff = log(left + 0.25) - log(right + 0.25);
-        float diff = log(left) - log(right);
-	//float diff = left - right;
-
-	printf("log diff: %f\n\n", diff);
-
-        // Verify the validity of the difference
-        if (!isnan(diff) && !isinf(diff)) {
-        
-            // Get pid response
-            float response = PID->pass(0.0f, diff);
-
-            // Set steering angle
-            picarx->setSteeringAngle(response);
-        
-        }
-
-	usleep(100000);
-
-    }
-
-}
-
-
-void test() {
-
-    picarx->setMotorSpeed(0.5f);
-    sleep(2);
-    picarx->setMotorSpeed(1.0f);
-    sleep(2);
-    picarx->setMotorSpeed(-0.5f);
-    sleep(2);
-    picarx->setMotorSpeed(-1.0f);
-    sleep(2);
-    picarx->setMotorSpeed(0.0f);
- 
-    for (float i=-30; i<=30; i+=10) {
-
-	    picarx->setSteeringAngle(i);
-	    sleep(2);
-
-    }
-
-}
-
-
-int main() {
-
-    // Register graceful exit handler
-    signal(SIGINT, gracefulExit);
-
-    // Create PiCarX object
-    picarx = new PiCarX();
-    PID    = new PIDController(15.0f, 0.0f, 0.0f, 0.1f);
-
-    // Connect to PiCarX
-    picarx->connect();
-
-    if (!picarx->isConnected()) {
-    
-        printf("Failed to connect to PiCarX\n");
-        return 1;
-    
-   }
-
-    // Set motor speed
-    picarx->setMotorSpeed(0.0f);
-    
-    // Set steering angle
-    picarx->setSteeringAngle(0.0f);
-
-
-    //test();
-    run();
-
-    if (picarx != NULL) {
-        picarx->disconnect();
-        delete picarx;
-        picarx = NULL;
-    }
-
-    if (PID != NULL) {
-        delete PID;
-        PID = NULL;
-    }
-
-
-    return 0;
-
-
-} 
